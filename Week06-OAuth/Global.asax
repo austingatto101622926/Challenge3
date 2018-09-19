422a5c71490d6b39c94f607ab6c3ffde43f0936a
Austin Gatto <austin.s.c.gatto@outlook.com>
2018-09-20 09:01:39 +1000

    Add project files.

---
+++ Week06-OAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week06_OAuth.MvcApplication" Language="C#" %>
---
+++ Week06-OAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week06_OAuth.Startup))]
+namespace Week06_OAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
